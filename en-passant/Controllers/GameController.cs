1b2fdfe1d80586f41866c1b6bbe2d4a0de06da8f
Marcela Nunes Zarichta <marcela.zarichta@edu.pucrs.br>
2025-07-24 14:33:18 -0300

    merge com a develop

--- en-passant/Controllers/GameController.cs
+++ en-passant/Controllers/GameController.cs
@@ -1,16 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using en_passant.Models;
 using en_passant.Models.Enum;
+using en_passant.Services.Interface;
 
 namespace en_passant.Controllers;
 
 public class GameController : Controller
 {
+    private IGameService _gameService;
+    
+    public GameController(IGameService gameService)
+    {
+        _gameService = gameService;
+    }
+
+    [HttpGet]
     public IActionResult AddJogo()
     {
         return View();
     }
-    
+
     public IActionResult EditGame()
     {
         var jogoMock = new Game
@@ -19,13 +28,20 @@ public class GameController : Controller
             Name = "Me da um tiro",
             Fornecedor = "aaaaaa",
             Year = new DateTime(2022, 1, 1),
-            MadeOf = Material.Wood,
+            MadeOf = Material.Plastic,
             Category = Category.Classic,
             GameType = GameType.Card,
             Description = "Jogo de tabuleiro."
         };
 
         return View(jogoMock);
+    }
+
+    [HttpPut]
+    public IActionResult EditGame(Game game)
+    {
+        _gameService.Update(game);
+        return RedirectToAction("Index", "Home");
     }
     
     public IActionResult ViewGame()
