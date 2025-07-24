ec39cbbb65f0e00e8b6c7f796ba0f074e4b3dccd
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-07-24 15:34:10 -0300

    stock controller made

---
+++ en-passant/Controllers/StockController.cs
@@ -0,0 +1,67 @@
+using en_passant.Models;
+using en_passant.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+public class StockController : Controller
+{
+    private readonly IStockService _stockService;
+
+    public StockController(IStockService stockService)
+    {
+        _stockService = stockService;
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public IActionResult AddStock(GameStock gameStock)
+    {
+        _stockService.Add(gameStock);
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("get")]
+    public IActionResult GetAllStocks()
+    {
+        var stocks = _stockService.GetAll();
+        return Ok(stocks);
+    }
+
+    [HttpGet]
+    [Route("get/{id}")]
+    public IActionResult GetStockById(long id)
+    {
+        var stock = _stockService.GetById(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+        return Ok(stock);
+    }
+
+    [HttpDelete]
+    [Route("delete/{id}")]
+    public IActionResult DeleteStock(long id)
+    {
+        var result = _stockService.Delete(id);
+        if (!result)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("update/{id}")]
+    public IActionResult UpdateStock(long id, int quantity)
+    {
+        _stockService.Update(id, quantity);
+        return Ok();
+    }
+
+
+
+
+
+}
