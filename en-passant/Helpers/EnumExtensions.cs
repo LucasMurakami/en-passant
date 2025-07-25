64ab0d2a2c9242d10f1f83486bb01e7b0bf46751
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-07-25 12:01:07 -0300

    Controles de input e mudanças na index e addjogo

---
+++ en-passant/Helpers/EnumExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        return value.GetType()
+                    .GetMember(value.ToString())
+                    .First()
+                    .GetCustomAttribute<DisplayAttribute>()?
+                    .Name ?? value.ToString();
+    }
+}
